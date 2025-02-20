dc2948e93872bf0f115ed1ca09aa631edc93d053
Christian Kraich <christiankraich@gmail.com>
2025-02-19 22:45:39 -0300

    atualizei o banco de dados

--- sistema_automotivov3.sql
+++ sistema_automotivov3.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clientes` (
-  `id_cliente` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `nome` varchar(30) NOT NULL,
   `sobrenome` varchar(30) NOT NULL,
   `data_nascimento` date DEFAULT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `clientes` (
   `bairro` varchar(20) DEFAULT NULL,
   `cidade` varchar(25) DEFAULT NULL,
   `estado` varchar(19) DEFAULT NULL,
-  PRIMARY KEY (`id_cliente`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `cpf` (`cpf`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -55,6 +55,35 @@ LOCK TABLES `clientes` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `faturamento`
+--
+
+DROP TABLE IF EXISTS `faturamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `faturamento` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_os` int(10) unsigned NOT NULL,
+  `data` date NOT NULL,
+  `valor_servico` decimal(8,2) NOT NULL,
+  `valor_pecas` decimal(8,2) NOT NULL,
+  `valor_liquido` decimal(9,2) GENERATED ALWAYS AS (`valor_servico` + `valor_pecas`) STORED,
+  PRIMARY KEY (`id`),
+  KEY `id_os` (`id_os`),
+  CONSTRAINT `faturamento_ibfk_1` FOREIGN KEY (`id_os`) REFERENCES `ordem_de_servico` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faturamento`
+--
+
+LOCK TABLES `faturamento` WRITE;
+/*!40000 ALTER TABLE `faturamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faturamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `fornecedores`
 --
 
@@ -62,10 +91,10 @@ DROP TABLE IF EXISTS `fornecedores`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `fornecedores` (
-  `id_fornecedor` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `nome` varchar(30) NOT NULL,
-  `email` varchar(40) DEFAULT NULL,
-  `cnpj` varchar(18) DEFAULT NULL,
+  `email` varchar(40) NOT NULL,
+  `cnpj` varchar(18) NOT NULL,
   `telefone` varchar(15) NOT NULL,
   `endereco` varchar(50) NOT NULL,
   `numero` smallint(5) unsigned NOT NULL,
@@ -74,7 +103,7 @@ CREATE TABLE `fornecedores` (
   `bairro` varchar(20) NOT NULL,
   `cidade` varchar(25) NOT NULL,
   `estado` varchar(19) NOT NULL,
-  PRIMARY KEY (`id_fornecedor`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `cnpj` (`cnpj`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -97,7 +126,7 @@ DROP TABLE IF EXISTS `funcionarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `funcionarios` (
-  `id_funcionario` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `nome` varchar(30) NOT NULL,
   `sobrenome` varchar(30) NOT NULL,
   `data_nascimento` date DEFAULT NULL,
@@ -114,7 +143,7 @@ CREATE TABLE `funcionarios` (
   `senha` varchar(20) DEFAULT NULL,
   `cargo` varchar(20) DEFAULT NULL,
   `nivel_acesso` enum('Administrador','Comum') DEFAULT NULL,
-  PRIMARY KEY (`id_funcionario`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `cpf` (`cpf`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -128,6 +157,97 @@ LOCK TABLES `funcionarios` WRITE;
 /*!40000 ALTER TABLE `funcionarios` DISABLE KEYS */;
 /*!40000 ALTER TABLE `funcionarios` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ordem_de_servico`
+--
+
+DROP TABLE IF EXISTS `ordem_de_servico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ordem_de_servico` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(10) unsigned NOT NULL,
+  `id_funcionario` int(10) unsigned NOT NULL,
+  `data_abertura` date NOT NULL,
+  `data_concluido` date DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `status` enum('Concluída','Pendente') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_funcionario` (`id_funcionario`),
+  CONSTRAINT `id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
+  CONSTRAINT `id_funcionario` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordem_de_servico`
+--
+
+LOCK TABLES `ordem_de_servico` WRITE;
+/*!40000 ALTER TABLE `ordem_de_servico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ordem_de_servico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pecas`
+--
+
+DROP TABLE IF EXISTS `pecas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pecas` (
+  `id` int(10) unsigned NOT NULL,
+  `id_fornecedor` int(10) unsigned NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `valor_unidade_fornecedor` decimal(8,2) DEFAULT NULL,
+  `valor_unidade_cliente` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_fornecedor` (`id_fornecedor`),
+  CONSTRAINT `id_fornecedor` FOREIGN KEY (`id_fornecedor`) REFERENCES `fornecedores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pecas`
+--
+
+LOCK TABLES `pecas` WRITE;
+/*!40000 ALTER TABLE `pecas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pecas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pecas_os`
+--
+
+DROP TABLE IF EXISTS `pecas_os`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pecas_os` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_peca` int(10) unsigned NOT NULL,
+  `id_os` int(10) unsigned NOT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_peca` (`id_peca`),
+  KEY `id_os` (`id_os`),
+  CONSTRAINT `pecas_os_ibfk_1` FOREIGN KEY (`id_peca`) REFERENCES `pecas` (`id`),
+  CONSTRAINT `pecas_os_ibfk_2` FOREIGN KEY (`id_os`) REFERENCES `ordem_de_servico` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pecas_os`
+--
+
+LOCK TABLES `pecas_os` WRITE;
+/*!40000 ALTER TABLE `pecas_os` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pecas_os` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -138,4 +258,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-16 19:08:43
+-- Dump completed on 2025-02-19 22:43:15
