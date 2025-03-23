eedcadf71420140a25f3396cdc3aab84ec8c9014
Christian Kraich <christiankraich@gmail.com>
2025-03-22 22:54:45 -0300

    removi salario da tabela cargos
    e também o insert into clientes

--- sistema-automotivov8.3.sql
+++ sistema-automotivov8.3.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS `cargos`;
 CREATE TABLE `cargos` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) NOT NULL,
-  `salario` decimal(7,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -33,7 +32,6 @@ CREATE TABLE `clientes` (
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 LOCK TABLES `clientes` WRITE;
-INSERT INTO `clientes` VALUES (1,'alles gut','1990-12-12','allesgut@yahoo.com.br','654.646.654-65','(47) 9 9558-1447','69852-211','rua dos alfinetes',3000,'casa 04','crimlho','sabatina','ES'),(4,'Jão Lima','2005-05-05','jaolima@hotmail.com','696.014.725-66','(47) 9 8896-5566','85274-247','Rua getulio vargas',111,'casa','guanabara','florianopolis','SC'),(5,'Arthur Zannetti','2001-11-11','arthurzan@gmail.com','124.555.555-55','(12) 9 6744-7744','12885-588','Rua dos imigrantes',122,'casa','Boemia','Recife','PE'),(6,'Mateus Boas Dores','1985-06-30','mboasdores@hotmail.com','222.222.222-22','(99) 9 6588-5690','69885-101','Rua dos colonos',2,'casa','boqueirao','Curitiba','PR'),(9,'jonata gamoes rodrigues','2002-12-22','jonatagr@','232.323.232-32','(22) 2 2222-2333','22222-233','rua da vitoria',2,'casa','Madureira','Rio de Janeiro','RJ'),(10,'Felipe Farias','2000-10-10','fefa@yahoo.com.br','352.114.205-55','(59) 9 9585-2112','80555-154','rua padre kolb',209,'apartamento 101','cubatao','sao paulo','SP');
 UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `faturamento`;
